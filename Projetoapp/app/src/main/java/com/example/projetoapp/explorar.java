a7f95457797fbf4817e606015061ce0780d80292
ViniciusDantz <vinicius.d.paranhos@gmail.com>
2019-06-02 02:14:33 -0300

    tela explorar e destaque prontas e infladas mas não testadas

--- Projetoapp/app/src/main/java/com/example/projetoapp/explorar.java
+++ Projetoapp/app/src/main/java/com/example/projetoapp/explorar.java
@@ -1,18 +1,31 @@
 package com.example.projetoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
-public class explorar extends AppCompatActivity {
+public class explorar extends AppCompatActivity implements View.OnClickListener {
     Button botao1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explorar);
-
+        botao1 = (Button)findViewById(R.id.btnexplorar);
+        botao1.setOnClickListener(this);
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnexplorar:
+                Intent i = new Intent(this, Telaexplorar.class);
+                startActivity(i);
+                break;
+        }
     }
 }
